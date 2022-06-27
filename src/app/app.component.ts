cd1d934b87cc807c066083cf3797cdba5147855a
Daniel Oto Diaz <danieloto212@gmail.com>
2022-06-26 21:24:04 -0300

    button up and down add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,22 @@ export class AppComponent {
   title = 'pre';  
   section2Active= false;
 
+  arrow:string="../assets/down-arrow .png"
+
+  bottomArrow:string = "../assets/down-arrow .png"
+  upArrow:string = "../assets/up-arrow.png";
+
    activeSection2(){
     this.section2Active = !this.section2Active
-    
+    if(this.arrow === this.bottomArrow){
+      return this.arrow = this.upArrow
+    }
+    else{
+     return this.arrow = this.bottomArrow
+    }
+  }
+
+  activeAboutMe(){
+    this.section2Active = true
   }
 }
