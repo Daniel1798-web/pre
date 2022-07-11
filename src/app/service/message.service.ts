f31c057a48d325b3dba902b0cacf609cce9386b4
Daniel Oto Diaz <danieloto212@gmail.com>
2022-07-11 17:53:48 -0300

    s

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -5,6 +5,6 @@ import { HttpClient } from '@angular/common/http';
 export class MessageService {
 constructor(private _http: HttpClient) { }
 sendMessage(textitos:any) {
- return this._http.post('https://back-pre.vercel.app/formulario', textitos);
+ return this._http.post('https://back-pre.vercel.app/send-mail', textitos);
  }
 }
