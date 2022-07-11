d10b1dccf3e9c61cde4979f640feb4ee7bb476df
Daniel Oto Diaz <danieloto212@gmail.com>
2022-07-11 19:35:26 -0300

    Update message.service.ts

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MessageService {
 constructor(private _http: HttpClient) { }
-sendMessage(textitos:any) {
- return this._http.post('https://back-pre.vercel.app/send-mail', textitos);
+sendMessage(mail:any):Observable<any> {
+ return this._http.post('https://back-pre.vercel.app/formulario', mail);
  }
 
 rec() {
