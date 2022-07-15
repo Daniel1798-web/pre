ebbbd32ddf907ff8fcfbd7f07d76c4bc8b6b45d1
Daniel Oto Diaz <danieloto212@gmail.com>
2022-07-15 16:20:16 -0300

    s

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -5,9 +5,10 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class MessageService {
 constructor(private _http: HttpClient) { }
+
 sendMessage(body:any){
     
- return this._http.post('https://back-pre.vercel.app/formulario', body);
+ return this._http.get('https://back-pre.vercel.app/formulario', body);
  }
 
 rec() {
